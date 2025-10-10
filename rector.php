b080700743581e4cda91f2790d8f15371232a3cc
Julien Waguet <julien.waguet@assoconnect.com>
2025-10-10 16:49:35 +0200

    Adds rector and upgrades phpunit
    Adds rector as a dev dependency to automate code refactoring.
    Also upgrades phpunit to the latest version.
    This helps to improve code quality and maintainability.

---
+++ rector.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ])
+    // uncomment to reach your current PHP version
+    // ->withPhpSets()
+    ->withTypeCoverageLevel(0)
+    ->withDeadCodeLevel(0)
+    ->withCodeQualityLevel(0)
+    ->withSets([
+        __DIR__ . '/vendor/assoconnect/php-quality-config/src/Rector/rules.php',
+    ]);
